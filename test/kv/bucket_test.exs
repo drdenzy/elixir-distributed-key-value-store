b629163c618f57a3fe4b3be884f10a5fab7de965
Dennis Akpenyi <dennisakpenyi@gmail.com>
2025-03-14 12:40:53 -0400

    Elixir Process module tour - replaced Agents with GenServer to better delineate between Client and Server functionality

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -2,7 +2,8 @@ defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, bucket} = KV.Bucket.start_link([])
+    bucket = start_supervised!(KV.Bucket)
+    # {:ok, bucket} = KV.Bucket.start_link([])
     %{bucket: bucket}
   end
 
